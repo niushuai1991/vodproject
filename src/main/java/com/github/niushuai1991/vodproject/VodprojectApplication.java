3145133b39d445ebac584a457698aa9631452f03
niushuai1991 <niushuai1991@foxmail.com>
2023-01-30 12:00:28 +0800

    init

---
+++ src/main/java/com/github/niushuai1991/vodproject/VodprojectApplication.java
@@ -0,0 +1,13 @@
+package com.github.niushuai1991.vodproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/niushuai1991/vodproject/VodprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.niushuai1991.vodproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
